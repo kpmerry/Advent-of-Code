5895818354776323cb41c4b5ba249a3f8d669708
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-12-01 21:21:37 +0000

    updated 1.py

--- 2024 Advent of Code/1/1.py
+++ 2024 Advent of Code/1/1.py
@@ -1,47 +1,39 @@
 # Day 1 Challenge - Python
-def ch1part1():
-    # Get input of two lists
+def ch1part1(filename):
+    # Extract lists from input file.
     list1 = []
     list2 = []
     list_len = 0
-
-    with open("lists.txt", "r") as fin:
+    with open(filename, "r") as fin:
         for line in fin:
             list_len += 1
             nums = line.split("   ")
             list1.append(int(nums[0]))
             list2.append(int(nums[1]))
-
-    # Sort lists
+    # Sort the lists.
     list1.sort()
     list2.sort()
-
-    # Add differences of each ordered item
+    # Add the absolute difference of the pairs of ordered items.
     diff = 0
-
     for i in range(list_len):
         diff += abs(list1[i] - list2[i])
-
     return diff
 
-def ch1part2():
-    # Get input of two lists
+def ch1part2(filename):
+    # Extract lists from input file.
     list1 = []
     list2 = []
     list_len = 0
-
-    with open("lists.txt", "r") as fin:
+    with open(filename, "r") as fin:
         for line in fin:
             nums = line.split("   ")
             list1.append(int(nums[0]))
             list2.append(int(nums[1]))
-    
+    # Find the similarity score (list1 item * item count in list2).
     sim_score = 0
-
     for i in list1:
         sim_score += i * list2.count(i)
-
     return sim_score
 
-print(ch1part1())
-print(ch1part2())
+print(ch1part1("lists.txt"))
+print(ch1part2("lists.txt"))
