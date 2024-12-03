82cffdc6bbd769eed4202a91037fe9c97bb8766f
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-12-03 16:38:06 +0000

    updated 3.py

--- 2024 Advent of Code/3/3.py
+++ 2024 Advent of Code/3/3.py
@@ -6,15 +6,6 @@ def extract_file_info(filename):
     return string
 
 
-def find_valid_char(s):
-    valid_s = ""
-    valid = "mul(,)"  # and numeric
-    for char in s:
-        if char in valid or char.isnumeric():
-            valid_s += char
-    return valid_s
-
-
 def find_valid_mul(s):
     product_pairs = []
     s_list = s.split("mul")
@@ -48,15 +39,43 @@ def get_products(pairs):
 
 def product_all_lines(filename):
     s = extract_file_info(filename)
-    # s1 = find_valid_char(s)
-    s2 = find_valid_mul(s)
-    product = get_products(s2)
+    s1 = find_valid_mul(s)
+    product = get_products(s1)
+    return product
+
+
+def enabled_muls(s):
+    enabled = "do()"
+    disabled = "don't()"
+    dos = s.split(enabled)
+    cut_offs = []
+    pairs = []
+    for j in range(len(dos)):
+        for i in range(len(dos[j]) - 7):
+            if dos[j][i : i + 7] == disabled:
+                cut_offs.append(i)
+        if len(cut_offs) != j + 1:
+            cut_offs.append(-1)
+    for k in range(len(dos)):
+        res = find_valid_mul(dos[k][: (cut_offs[k])])
+        pairs.append(res)
+    return pairs
+
+
+def product_enabled(filename):
+    product = 0
+    s = extract_file_info(filename)
+    s2 = enabled_muls(s)
+    for string in s2:
+        prod = get_products(string)
+        product += prod
     return product
 
 
 def main():
     print("Day 3:")
     print(product_all_lines("input.txt"))
+    print(product_enabled("example2.txt"))
 
 
 if __name__ == "__main__":
