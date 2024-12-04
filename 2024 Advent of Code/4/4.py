a2d0eae75bfd74783b1ff9dd087b260290e6bbc8
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-12-04 17:22:01 +0000

    updated 4.py

--- 2024 Advent of Code/4/4.py
+++ 2024 Advent of Code/4/4.py
@@ -2,12 +2,12 @@ def read_file(filename):
     info = []
     with open(filename, "r") as fin:
         for line in fin:
-            info.append(line.strip())
+            info.append(list(line.strip()))
     return info
 
 
 def find_words(wordsearch):
-    count = 0
+
     # Where wordsearch is m x n rows x cols
     m = len(wordsearch)
     n = len(wordsearch[0])
@@ -20,12 +20,29 @@ def find_words(wordsearch):
     word = "xmas"
 
     dirs = [[1, 1], [1, 0], [0, 1], [0, -1], [-1, 0], [-1, -1], [1, -1], [-1, 1]]
+    count = 0
 
-    return count
+    for i in range(m):
+        for j in range(n):
+            if wordsearch[i][j] != letter:
+                continue
+            for di in dirs:
+                word_check = ""
+                try:
+                    word_check += wordsearch[i][j]
+                    word_check += wordsearch[i + dir[0]][j + dir[1]]
+                    word_check += wordsearch[i + (2 * dir[0])][j + (2 * dir[1])]
+                    word_check += wordsearch[i + (3 * dir[0])][j + (3 * dir[1])]
+                    if word_check == "xmas" or word_check == "samx":
+                        count += 1
+                except:
+                    continue
+        return count
 
 
 def part_one(filename):
     wordsearch = read_file(filename)
+    print(wordsearch)
     count = find_words(wordsearch)
     return count
 
