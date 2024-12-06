8bee5957ec369fa6c74798ea6c4139a27a4c4303
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-12-06 12:15:11 +0000

    started 6.py challenge

---
+++ 2024 Advent of Code/6/6.py
@@ -0,0 +1,66 @@
+def extract_environment(filename):
+    env = []
+    with open(filename, "r") as fin:
+        for line in fin:
+            env.append(list(line.strip()))
+    return env
+
+
+def part_one(filename):
+    env = extract_environment(filename)
+    guard = "^"
+    queue = []
+
+    for i in range(len(env)):
+        for j in range(len(env[0])):
+            if env[i][j] == guard:
+                queue.append([i, j])  # row, col
+
+    dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+    direction = 0
+    count = 0
+
+    while queue:
+        current = queue.pop(0)
+        env[current[0]][current[1]] = "X"
+        """
+        if (
+            current[0] < 0
+            or current[0] >= len(env)
+            or current[1] < 0
+            or current[1] >= len(env)
+        ):
+            break
+        """
+        try:
+            next_cell = [
+                current[0] + dirs[direction][0],
+                current[1] + dirs[direction][1],
+            ]
+            if env[next_cell[0]][next_cell[1]] == "#":
+                direction += 1
+                if direction >= 4:
+                    direction %= 4
+                queue.append(
+                    [current[0] + dirs[direction][0], current[1] + dirs[direction][1]]
+                )
+            else:
+                queue.append(next_cell)
+        except:
+            break
+
+    for a in range(len(env)):
+        for b in range(len(env[0])):
+            if env[a][b] == "X" or env[a][b] == "^":
+                count += 1
+
+    return count
+
+
+def main():
+    print(part_one("input.txt"))
+
+
+if __name__ == "__main__":
+    main()
