04f5fc79648d467dc1d6544c5dcaca21b24142e9
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-12-08 22:42:07 +0000

    update 6.py

--- 2024 Advent of Code/6/6.py
+++ 2024 Advent of Code/6/6.py
@@ -46,59 +46,6 @@ def mark_path(env):
     return env
 
 
-def is_valid(env, point):
-    if point[0] < 0 or point[1] < 0 or point[0] >= len(env) or point[1] >= len(env[0]):
-        return False
-    return True
-
-
-def find_cycle(env, point):
-
-    if not is_valid(env, point) or env[point[0]][point[1]] == "#":
-        return False
-
-    dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    count = 0
-
-    for i in range(len(dirs)):
-        di = i
-        env[point[0]][point[1]] = "#"
-        start = [point[0] + dirs[di][0], point[1] + dirs[di][1]]
-
-        visited = []  # (x coord, y coord, direction)
-
-        if not is_valid(env, start):
-            continue
-
-        queue = [start]
-        di = (di - 1) % 4
-
-        while queue:
-            count += 1
-
-            current = queue.pop(0)
-
-            next_cell = [current[0] + dirs[di][0], current[1] + dirs[di][1]]
-            if not is_valid(env, next_cell):
-                break
-
-            if (next_cell, di) == (point, i):
-                return True
-
-            elif env[next_cell[0]][next_cell[1]] == "#":
-                if (current[0], current[1], di) not in visited:
-                    visited.append((current[0], current[1], di))
-                else:
-                    return True
-                di = (di + 1) % 4
-                next_cell = [current[0] + dirs[di][0], current[1] + dirs[di][1]]
-                if env[next_cell[0]][next_cell[1]] == "#":
-                    continue
-            queue.append(next_cell)
-
-    return False
-
-
 def part_one(filename):
     """Identify how many distinct cells the path will visit."""
     env = mark_path(extract_environment(filename))
@@ -114,28 +61,102 @@ def part_one(filename):
     return count
 
 
+def record_path(env):
+    guard = "^"
+    queue = []
+    data = []
+    guard_point = []
+
+    # Find starting position.
+    for i in range(0, len(env)):
+        for j in range(0, len(env[0])):
+            if env[i][j] == guard:
+                queue.append([i, j])  # row, col
+                guard_point = [i, j]
+
+    dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    direction = 0
+
+    # Visit each point in route and mark in environment with an "X".
+    while queue:
+        current = queue.pop(0)
+        data.append((current[0], current[1], direction))
+
+        next_cell = [
+            current[0] + dirs[direction][0],
+            current[1] + dirs[direction][1],
+        ]
+        # If route will go off grid, end the loop.
+        if (
+            next_cell[0] < 0
+            or next_cell[0] >= len(env)
+            or next_cell[1] < 0
+            or next_cell[1] >= len(env)
+        ):
+            break
+        # If the route hits an obstacle, shift direction clockwise.
+        elif env[next_cell[0]][next_cell[1]] == "#":
+            direction = (direction + 1) % 4
+        queue.append([current[0] + dirs[direction][0], current[1] + dirs[direction][1]])
+    return data
+
+
+def find_cycle(point_data, env, point_before):
+
+    px, py, pdi = point_data
+    sx, sy, sdi = point_before
+    env[px][py] = "#"
+    queue = []
+
+    dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    di = sdi
+
+    queue.append([sx, sy])
+    visited_corners = []
+
+    while queue:
+        cx, cy = queue.pop(0)
+        if cx < 0 or cx >= len(env) or cy < 0 or cy >= len(env[0]):
+            return False
+        nx, ny = cx + dirs[di][0], cy + dirs[di][1]
+        if nx < 0 or nx >= len(env) or ny < 0 or ny >= len(env[0]):
+            return False
+        if (nx, ny, di) == (sx, sy, sdi):
+            return True
+        if (cx, cy, di) in visited_corners:
+            return True
+        if env[nx][ny] == "#":
+            visited_corners.append((cx, cy, di))
+            di = (di + 1) % 4
+            nx, ny = cx + dirs[di][0], cy + dirs[di][1]
+        # Again for corners.
+        if env[nx][ny] == "#":
+            di = (di + 1) % 4
+            nx, ny = cx + dirs[di][0], cy + dirs[di][1]
+        if (nx, ny, di) == (px, py, pdi):
+            return True
+        queue.append([nx, ny])
+
+    return False
+
+
 def part_two(filename):
     """Identify where to place obstacles to create a cycling path."""
-    environment = mark_path(extract_environment(filename))
+    env = extract_environment(filename)
+    path_data = record_path(env)
     count = 0
-
-    for i in range(len(environment)):
-        for j in range(len(environment[0])):
-            if environment[i][j] != "X":
-                continue
-            if find_cycle([row[:] for row in environment], [i, j]):
-                count += 1
-
-    count -= 1  # Remove guard starting posiiton
+    for i in range(1, len(path_data)):
+        if find_cycle(path_data[i], [row[:] for row in env], path_data[i - 1]):
+            count += 1
     return count
 
 
-def main():
+def main(filename):
     print("Day 6:")
-    print((part_one("input.txt")))
-    res = part_two("input.txt")
+    print((part_one(filename)))
+    res = part_two(filename)
     print(res)
 
 
 if __name__ == "__main__":
-    main()
+    main("input.txt")
