0dc3f225c45135dcd2afeca8774cabae4d206a7b
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-12-09 11:19:02 +0000

    update 9.py correct part one answers

--- 2024 Advent of Code/9/9.py
+++ 2024 Advent of Code/9/9.py
@@ -5,22 +5,24 @@ def part_one(filename):
         for line in fin:
             data += line.strip()
 
-    file_system = ""
+    file_system = []
 
     for i in range(len(data)):
         if i % 2 == 0:
-            file_system += str(i // 2) * int(data[i])
+            for j in range(int(data[i])):
+                file_system.append(str(i // 2))
         else:
-            file_system += "." * int(data[i])
+            for k in range(int(data[i])):
+                file_system.append(".")
 
     j = len(file_system) - 1
     count = 0
-    dot_count = file_system.count(".")
 
     for index in range(0, len(file_system)):
         # If j is less than current index.
         if (j) < index:
             break
+
         if file_system[index] == ".":
             while file_system[j] == ".":
                 j -= 1
